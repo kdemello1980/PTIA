1374bd203dd3fc465f7589db5eeeb7e6c5f62e93
Ken de Mello <k6demello28@fastmail.fm>
2023-08-28 16:27:17 -0400

    Begin fleshing out Consume()

--- Assets/Scripts/Actors/Actor.cs
+++ Assets/Scripts/Actors/Actor.cs
@@ -42,8 +42,85 @@ public class Actor : MonoBehaviour
     }
 
     /// <summary>Consume() is called by the collision of 2 Actors and determines what happens with each.</summary>
-    public virtual void Consume()
+    public virtual void Consume(Actor prey)
     {
-
+        // Consumption rules:
+        // • When both actors are non toxic
+        //     ◦ The larger actor always consumes all of the smaller
+        //     ◦ If they are exactly the same size they bounce off
+        // • When one is toxic and the other is non - toxic
+        //     ◦ If the larger one is non - toxic, then the volume of the toxic one is subtracted
+        //     ◦ if they are the same size or the non - toxic one is smaller, the non-toxic one is annihilated.
+        if (!prey.IsToxic && !IsToxic)
+        {
+            // prey absorbs
+            if (prey.ActorVolume > ActorVolume)
+            {
+                prey.ActorVolume += ActorVolume;
+                if (!gameObject.CompareTag("Player"))
+                {
+                    Destroy(gameObject);
+                }
+            }
+            // absorbs prey
+            else if (prey.ActorVolume <= ActorVolume)
+            {
+                ActorVolume += prey.ActorVolume;
+                if (!prey.gameObject.CompareTag("Player"))
+                {
+                    Destroy(prey.gameObject);
+                }
+            }
+            // they bounce off
+            else if (prey.ActorVolume == ActorVolume)
+            {
+            }
+        }
+        else if (prey.IsToxic && IsToxic)
+        {
+            if (prey.ActorVolume > ActorVolume)
+            {
+            }
+            else if (prey.ActorVolume <= ActorVolume)
+            {
+            }
+            else if (prey.ActorVolume == ActorVolume)
+            {
+            }
+        }
+        else if (prey.IsToxic && !IsToxic)
+        {
+            if (prey.ActorVolume > ActorVolume)
+            {
+            }
+            else if (prey.ActorVolume <= ActorVolume)
+            {
+            }
+            else if (prey.ActorVolume == ActorVolume)
+            {
+            }
+        }
+        else if (!prey.IsToxic && IsToxic)
+        {
+            if (prey.ActorVolume > ActorVolume)
+            {
+            }
+            else if (prey.ActorVolume <= ActorVolume)
+            {
+            }
+            else if (prey.ActorVolume == ActorVolume)
+            {
+            }
+        }
     }
+
+
+    /// <summary>Trigger Consume() when a Rigidbody collision is entered.</summary>
+    void OnCollisionEnter(Collision other)
+    {
+        // Just a reminder how to get at our actor object
+        // float volume = other.gameObject.GetComponent<Actor>().ActorVolume;
+        Consume(other.gameObject.GetComponent<Actor>());
+    }
+
 }
