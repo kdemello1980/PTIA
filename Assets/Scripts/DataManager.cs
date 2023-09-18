3c5a0209db8087e2f2a8479d6f3448e770e17c54
Kenneth de Mello <k6demello28@fastmail.fm>
2023-09-18 16:22:10 -0400

    move scene controllers

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -23,6 +23,9 @@ public class DataManager : MonoBehaviour
     public string SettingsScene { get; protected set; } = "SettingsScene"; // ENCAPSULATION
     public string HowtoScene { get; protected set; } = "HowtoScene"; // ENCAPSULATION
 
+    // Music file
+    public string BackgroundMusicPath { get; set;}
+
     // Game field size
     public float GameFieldSize { get; set; } = 250.0f;
 
